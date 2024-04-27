a521cc9376a5864d1337c9f6cb6356ce7997db66
Levi9111 <captainlevi9111@gmail.com>
2024-04-27 13:07:52 +0600

    upcoming meeting timing fixed

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,4 +1,6 @@
+'use client';
 import MeetingTypeList from '@/components/MeetingTypeList';
+import { useGetCalls } from '@/hooks/useGetCalls';
 import React from 'react';
 
 const Home = () => {
@@ -12,12 +14,24 @@ const Home = () => {
     now,
   );
 
+  const { upcomingCalls } = useGetCalls();
+
+  const nextMeeting = upcomingCalls[
+    upcomingCalls.length - 1
+  ]?.state.startsAt?.toLocaleTimeString([], {
+    hour: 'numeric',
+    minute: '2-digit',
+    hour12: true,
+  });
+
   return (
     <section className='flex size-full flex-col gap-10 text-white'>
       <div className='h-[300px] w-full rounded-[20px] bg-hero bg-cover'>
         <div className='flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11'>
           <h2 className='glassmorphism max-w-[270px] rounded py-2 text-center text-base font-normal'>
-            Upcoming Meeting at: 1230pm
+            {upcomingCalls.length > 0
+              ? `Upcoming Meeting at: ${nextMeeting}`
+              : 'No meeting Scheduled'}
           </h2>
           <div className='flex flex-col gap-2 '>
             <h1 className='text-4xl font-extrabold lg:text-7xl'>{time}</h1>
