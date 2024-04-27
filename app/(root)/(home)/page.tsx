6f03f7c9ada90dee66529a7a3b32c013e1359fda
Levi9111 <captainlevi9111@gmail.com>
2024-04-27 12:46:56 +0600

    time date updated

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -2,17 +2,16 @@ import MeetingTypeList from '@/components/MeetingTypeList';
 import React from 'react';
 
 const Home = () => {
-  const time = new Date().toLocaleTimeString('en-US', {
+  const now = new Date();
+
+  const time = now.toLocaleTimeString('en-US', {
     hour: '2-digit',
     minute: '2-digit',
-    hour12: true,
   });
-  const date = new Date().toLocaleDateString('en-US', {
-    weekday: 'long',
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-  });
+  const date = new Intl.DateTimeFormat('en-US', { dateStyle: 'full' }).format(
+    now,
+  );
+
   return (
     <section className='flex size-full flex-col gap-10 text-white'>
       <div className='h-[300px] w-full rounded-[20px] bg-hero bg-cover'>
