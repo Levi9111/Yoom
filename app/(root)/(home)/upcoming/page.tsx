609fa459f6cbe2939bef59b4cd9380545dce36d1
Levi9111 <captainlevi9111@gmail.com>
2024-04-27 12:06:09 +0600

    project completed

--- app/(root)/(home)/upcoming/page.tsx
+++ app/(root)/(home)/upcoming/page.tsx
@@ -1,9 +1,11 @@
+import CallList from '@/components/CallList';
 import React from 'react';
 
 const Home = () => {
   return (
     <section className='flex size-full flex-col gap-10 text-white'>
       <h3 className='text-3xl font-bold'>Upcoming</h3>
+      <CallList type='upcoming' />
     </section>
   );
 };
--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -20,13 +20,14 @@ import {
 
 import React, { useState } from 'react';
 import { LayoutList, Users } from 'lucide-react';
-import { useSearchParams } from 'next/navigation';
+import { useRouter, useSearchParams } from 'next/navigation';
 import EndCallButton from './EndCallButton';
 import Loader from './Loader';
 
 type CallLayoutType = 'grid' | 'speaker-left' | 'speaker-right';
 
 const MeetingRoom = () => {
+  const router = useRouter();
   const searchParams = useSearchParams();
   const isPersonalRoom = !!searchParams.get('personal');
   const [layout, setLayout] = useState<CallLayoutType>('speaker-left');
@@ -63,7 +64,7 @@ const MeetingRoom = () => {
       </div>
 
       <div className='fixed bottom-0 flex-center w-full gap-5 flex-wrap'>
-        <CallControls />
+        <CallControls onLeave={() => router.push('/')} />
         <DropdownMenu>
           <div className='flex items-center'>
             <DropdownMenuTrigger className='cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]'>
